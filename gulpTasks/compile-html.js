fb15138864f6077b025b2517cff7bcbb9badd7b6
Richard Johnston <richard.johnston@codecomputerlove.com>
2016-02-07 21:01:27 +0000

    feat : add contenful

--- gulpTasks/compile-html.js
+++ gulpTasks/compile-html.js
@@ -12,19 +12,36 @@ var templateHelpers = require('../_config/templateHelpers.js')();
 module.exports = function(gulp) {
 
     gulp.task('compile-html', function () {
-        var templateData = {
-            data: templateDataJson
-        },
+			// Don't require if you've already included contentful as a script tag
+			var contentful = require('contentful');
+			var client = contentful.createClient({
+			  accessToken: '846c64678f0b76bb5610f7dc0c9614bf6daf368bdc593099005ac0cfcd0712ea',
+			  space: 'xw6rzdftxrbl'
+			});
 
-        options = {
-            batch : handlebarsConfig.partials,
-            helpers: templateHelpers
-        }
-        
-        return gulp.src(handlebarsConfig.views)
-            .pipe(handlebars(templateData, options))
-            .pipe(rename({extname: '.html'}))
-            .pipe(gulp.dest('build'));
+			var page = '';
+
+			var templateData = {}
+
+			client.entry('2nRTiGAvFaCEsWMoUoKIcc')
+				.then(function (entry) {
+
+					options = {
+	            batch : handlebarsConfig.partials,
+	            helpers: templateHelpers
+	        }
+
+	        return gulp.src(handlebarsConfig.views)
+	            .pipe(handlebars(entry, options))
+	            .pipe(rename({extname: '.html'}))
+	            .pipe(gulp.dest('build'));
+				})
+
+
+
+
+
+
     });
 
 }
