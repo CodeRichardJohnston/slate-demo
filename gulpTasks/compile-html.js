2b59165cd7850897ef1358504fae9da604e554a2
Richard Johnston <richard.johnston@codecomputerlove.com>
2016-02-19 16:41:12 +0000

    Revert "feat : add contenful"
    This reverts commit fb15138864f6077b025b2517cff7bcbb9badd7b6.

--- gulpTasks/compile-html.js
+++ gulpTasks/compile-html.js
@@ -12,36 +12,19 @@ var templateHelpers = require('../_config/templateHelpers.js')();
 module.exports = function(gulp) {
 
     gulp.task('compile-html', function () {
-			// Don't require if you've already included contentful as a script tag
-			var contentful = require('contentful');
-			var client = contentful.createClient({
-			  accessToken: '846c64678f0b76bb5610f7dc0c9614bf6daf368bdc593099005ac0cfcd0712ea',
-			  space: 'xw6rzdftxrbl'
-			});
+        var templateData = {
+            data: templateDataJson
+        },
 
-			var page = '';
-
-			var templateData = {}
-
-			client.entry('2nRTiGAvFaCEsWMoUoKIcc')
-				.then(function (entry) {
-
-					options = {
-	            batch : handlebarsConfig.partials,
-	            helpers: templateHelpers
-	        }
-
-	        return gulp.src(handlebarsConfig.views)
-	            .pipe(handlebars(entry, options))
-	            .pipe(rename({extname: '.html'}))
-	            .pipe(gulp.dest('build'));
-				})
-
-
-
-
-
-
+        options = {
+            batch : handlebarsConfig.partials,
+            helpers: templateHelpers
+        }
+        
+        return gulp.src(handlebarsConfig.views)
+            .pipe(handlebars(templateData, options))
+            .pipe(rename({extname: '.html'}))
+            .pipe(gulp.dest('build'));
     });
 
 }
